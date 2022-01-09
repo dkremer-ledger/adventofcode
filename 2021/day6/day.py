9a5515debc4983ee0e9a0b837d8a55af2f8c5f59
David Kremer <courrier@david-kremer.fr>
2022-01-09 10:53:02 +0100

    refactor day6

--- 2021/day6/day.py
+++ 2021/day6/day.py
@@ -1,11 +1,18 @@
 from typing import TypeAlias, List
 from collections import Counter
+
+import pytest
+
 Board: TypeAlias = List
 
-pop = Counter(map(int, open('day6.txt').read().split(',')))
+
+@pytest.fixture
+def input_data():
+    return Counter(map(int, open('day6.txt').read().split(',')))
+
+
 def evolve_pop(pop):
     new_pop = Counter()
-    new_fishes = 0
     for key, value in pop.items():
         if key == 0:
             new_pop[8] += value
@@ -14,11 +21,17 @@ def evolve_pop(pop):
             new_pop[key - 1] += pop[key]
     return new_pop
 
-for day in range(256):
-    pop = evolve_pop(pop)
+
+def evolve(pop, n: int = 80):
+    for day in range(n):
+        pop = evolve_pop(pop)
+    return sum(pop.values())
 
 
-print(sum(pop.values()))
+def test_part_1(input_data):
+    assert evolve(input_data, 80) == 374927
 
-print("### PART 1 ###")
-print("### PART 2 ###")
+
+def test_part_2(input_data):
+    assert evolve(input_data, 256) == 1687617803407
+
